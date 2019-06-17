b11adb284edb281214c11d110224079c0f811986
samer1193 <samer1193@gmail.com>
2019-06-16 21:34:53 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Navbar from './Components/layout/Navbar';
+import Users from './Components/users/Users';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
